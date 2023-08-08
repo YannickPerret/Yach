a7362c0591026e1cc0f4f60351eb9e726a186324
YannickPerret <contact@yannickperret.com>
2023-08-08 15:06:23 +0200

    change get calendar in web and submit for create subcalendar

--- app/class/webserver.js
+++ app/class/webserver.js
@@ -55,11 +55,11 @@ class Webserver {
 
         this.app.addContentTypeParser('text/calendar', { parseAs: 'string' }, function (req, body, done) {
             try {
-              done(null, body) // Here, body will be the raw text/calendar data
+                done(null, body) // Here, body will be the raw text/calendar data
             } catch (err) {
-              done(err)
+                done(err)
             }
-          });          
+        });
     }
 
     initRoutes() {
@@ -91,7 +91,7 @@ class Webserver {
 
         if (calendarData === null) {
             sharedCalendarData = await SharedCalendar.getById(id);
-            if (!sharedCalendarData) {
+            if (sharedCalendarData === null) {
                 return res.status(404).send({ error: 'No calendar found' });
             }
         }
@@ -101,67 +101,46 @@ class Webserver {
         res.type('Content-Type', 'text/calendar');
         res.status(200).send(comp.toString());
     }
-    
+
     async propfindCalendar(req, res) {
         let id = req.params.id;
         let sharedCalendarData;
-    
+
         console.log("PROPFIND request received for calendar:", id);
-    
-        let calendarData = await Calendar.getById(id);
-    
-        if (calendarData === null) {
-            sharedCalendarData = await SharedCalendar.getById(id);
-            if (!sharedCalendarData) {
-                return res.status(404).send({ error: 'No calendar found' });
-            }
-        }
-    
-        // Assuming the calendar data would be in a specific format for PROPFIND
-        let propfindData = {
-            // Sample data structure for PROPFIND. Adjust this as per your needs.
-            calendarId: id,
-            properties: {
-                // Define any specific properties you'd like to return for a PROPFIND request
-                name: calendarData ? calendarData.name : sharedCalendarData.name,
-                lastModified: calendarData ? calendarData.lastModified : sharedCalendarData.lastModified,
-                // ... any other properties you'd want
-            }
-        }
-    
+
         // set the correct headers
         res.type('Content-Type', 'text/calendar');
         res.status(207).send();  // 207 Multi-Status is commonly used for PROPFIND responses
     }
 
-    
     async updateCalendar(req, reply) {
-        console.log("Update calendar request received", req.body);
+        console.log("Update calendar request received");
 
-        // si l'id existe déjà il faut l'update
-        // si l'id n'existe pas il faut le créer dans les deux calendriers
-        // Comment savoir si on a supprimé un event ? 
-        //  - Récupérer les events des deux calendriers partagé et match avec les modifications. Si l'event existe en database, mais pas dans l'update : le supprimer en database
-        
         let id = req.params.id;
-        let body = req.body;
-        
+        let newEvents = req.body;
+        let sharedCalendar;
+
         try {
             let calendar = await Calendar.getById(id);
-            
-            if (!calendar) {
-                reply.status(404).send({ error: 'Calendar not found' });
-                return;
+            if (calendar === null) {
+                sharedCalendar = await SharedCalendar.getById(id);
+                if (sharedCalendar === null) {
+                    return reply.status(404).send({ error: 'No calendar found' });
+                }
+                else {
+                    for (let calendar of sharedCalendar.calendars) {
+                        console.debug(calendar.getEvents())
+
+                    }
+                }
             }
-            
-            // Update calendar attributes here
-            calendar.name = req.body.name;
-            // Add other fields as required
-            
-            await calendar.save(); // Assuming you have a save method on your Calendar model
-            
+
+
+
+
             reply.status(200).send(calendar);
         } catch (error) {
+            console.error(error);
             reply.status(500).send({ error: 'Server error' });
         }
     }
@@ -169,78 +148,75 @@ class Webserver {
     async submitCalendar(req, reply) {
         const data = req.file;
         const inputCalendarsSelected = req.body.calendars;
-        const typeCalendar = req.body.type === "PROFESSIONAL" || req.body.type === "PERSONAL" || req.body.type === "OTHER" ? req.body.type : "OTHER";
-        const nameCalendar = req.body.name ? req.body.name : "Yoda Default";
+        const typeCalendar = ["PROFESSIONAL", "PERSONAL", "OTHER"].includes(req.body.type) ? req.body.type : "OTHER";
+        const nameCalendar = req.body.name || "Yoda Default";
 
         let selectedCalendars = [];
         let filePath;
-        let outpuCalendar;
+        let outputCalendar;
 
-        if (inputCalendarsSelected && inputCalendarsSelected.length > 0) {
+        if (inputCalendarsSelected && Array.isArray(inputCalendarsSelected) && inputCalendarsSelected.length > 0) {
             selectedCalendars = inputCalendarsSelected;
         }
-        
+
         if (data) {
             filePath = data.path;
-            console.log('File upload finished successfully');
 
-            let uploadCalendar = new Calendar({ source: new FileAdapter({ fileName: filePath, encoding: 'utf8' }), format: 'ics', type: typeCalendar, name: nameCalendar});
+            const uploadCalendar = new Calendar({
+                source: new FileAdapter({ fileName: filePath, encoding: 'utf8' }),
+                format: 'ics',
+                type: typeCalendar,
+                name: nameCalendar
+            });
+
             await uploadCalendar.persist();
-
             await uploadCalendar.parseEvents();
 
-            for (let event of uploadCalendar.events) {
+            for (const event of uploadCalendar.events) {
                 event.calendarId = uploadCalendar.id;
                 await event.persist();
             }
-            
+
             selectedCalendars.push(filePath);
-            console.log("one calendar upload")
 
-            outpuCalendar = uploadCalendar
+            outputCalendar = uploadCalendar;
         }
 
-        if (Array.isArray(selectedCalendars) && selectedCalendars.length > 0) {
-            if (selectedCalendars.length > 1) {
-                let newSharedCalendar = new SharedCalendar({ name: nameCalendar });
+        if (Array.isArray(selectedCalendars) && selectedCalendars.length > 1) {
+            const newParentCalendar = new Calendar({ name: nameCalendar, type: "SHARED" });
+            await newParentCalendar.persist();
 
-                for (let selectedCalendar of selectedCalendars) {
-                    let calendar = await Calendar.getById(selectedCalendar)
-                    await newSharedCalendar.add(calendar);
-                }
-                await newSharedCalendar.persist();
-
-                console.log("multiple calendar upload")
-                outpuCalendar = newSharedCalendar;
+            for (const selectedCalendar of selectedCalendars) {
+                const calendar = await Calendar.getById(selectedCalendar);
+                calendar.parentCalendarId = newParentCalendar.id;
+                await calendar.persist();
             }
 
-            if (data) {
-                fs.unlink(filePath, (err) => {
-                    if (err) {
-                        console.error(err)
-                        return
-                    }
-                }
-                )
-            }
+            outputCalendar = newParentCalendar;
+        } else if (!data) {
+            return reply.status(400).send({ error: 'No calendars selected or uploaded' });
         }
-        else
-            return reply.status(400).send({ error: 'No calendars selected' });
 
-        reply.send({ url: `${process.env.ENDPOINT_URL}:${process.env.ENDPOINT_PORT}/api/v1/calendar/${outpuCalendar.id}` });
+        if (data) {
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+            });
+        }
+
+        reply.send({ url: `${process.env.ENDPOINT_URL}:${process.env.ENDPOINT_PORT}/api/v1/calendar/${outputCalendar.id}` });
     }
-
     // Static views home 
     async getHome(req, reply) {
 
         //get all calendar by database Calendar
         let calendars = await Calendar.getAll();
-        let sharedCalendars = await SharedCalendar.getAll();
 
         return reply.status(200).view('index.ejs', {
             title: 'Home Page',
             calendars: calendars,
-            sharedCalendars: sharedCalendars
         });
     }
 }
--- app/index.js
+++ app/index.js
@@ -2,7 +2,6 @@
 
 /************ Import / include  *****************/
 const Webserver = require('./class/webserver');
-const Calendar = require('./class/calendar');
 const FileAdapter = require('./class/fileAdapter');
 const yaml = require('js-yaml');
 require('dotenv').config();
@@ -14,26 +13,4 @@ let configFile = new FileAdapter({fileName: 'config.yaml', encoding: 'utf8'});
 
 let config = yaml.load(configFile.load());
 
-//load file calendar
-/*let fileCalendarEpsitec = new FileAdapter({fileName: config['Calendar Shared'].Epsitec, encoding: 'utf8'})
-let fileCalendarPerso = new FileAdapter({fileName: config.source, encoding: 'utf8'})
-
-
-// create calendar by file
-let calendarEpsitec = new Calendar({source : fileCalendarEpsitec, format: fileCalendarEpsitec.getExtension()});
-//calendarEpsitec.parseEvents();
-
-//calendarEpsitec.generate()
-let calendarPerso = new Calendar({source : fileCalendarPerso, format: fileCalendarPerso.getExtension()});
-
-*/
 let webSever = new Webserver({port: process.env.ENDPOINT_PORT, fileConfig: config});
-
-
-/*
-try {
-    let calendar = new Calendar({source: })
-
-} catch (error) {
-    console.error(error);
-}*/
