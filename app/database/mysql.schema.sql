2d15fe7ff6542c77f8551401a1cd54755a7cdd79
YannickPerret <contact@yannickperret.com>
2023-08-11 15:41:34 +0200

    removed unused file

--- app/database/mysql.schema.sql
+++ app/database/mysql.schema.sql
@@ -1,54 +1,41 @@
+-- Création de la table Calendar
 CREATE TABLE `Calendar` (
-    `id` VARCHAR(255) PRIMARY KEY,
-    `name` VARCHAR(255) NULL,
-    `type` VARCHAR(255) NOT NULL DEFAULT 'OTHER',
-    `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `id` CHAR(36) NOT NULL PRIMARY KEY,
+    `name` VARCHAR(255) DEFAULT NULL,
+    `type` VARCHAR(255) DEFAULT 'OTHER',
+    `createdAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `parentCalendarId` CHAR(36) DEFAULT NULL,
+    FOREIGN KEY (`parentCalendarId`) REFERENCES `Calendar`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    INDEX (`id`)
 );
 
-CREATE TABLE `SharedCalendar` (
-    `id` VARCHAR(255) PRIMARY KEY,
-    `name` VARCHAR(255) NULL,
-    `type` VARCHAR(255) NOT NULL DEFAULT 'OTHER',
-    `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE `SharedCalendarAssociation` (
-    `sharedCalendarId` VARCHAR(255) NOT NULL,
-    `calendarId` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`sharedCalendarId`, `calendarId`),
-    FOREIGN KEY (`sharedCalendarId`) REFERENCES `SharedCalendar`(`id`),
-    FOREIGN KEY (`calendarId`) REFERENCES `Calendar`(`id`)
-);
-
-CREATE INDEX `idx_sharedCalendarId` ON `SharedCalendarAssociation` (`sharedCalendarId`);
-
+-- Création de la table Event
 CREATE TABLE `Event` (
-    `id` VARCHAR(255) PRIMARY KEY,
-    `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `id` CHAR(36) NOT NULL PRIMARY KEY,
+    `createdAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `summary` VARCHAR(255) NOT NULL,
-    `sequence` INT NULL,
-    `status` VARCHAR(255) NULL,
-    `transp` VARCHAR(255) NULL,
-    `rRule` VARCHAR(255) NULL,
+    `sequence` INT DEFAULT NULL,
+    `status` VARCHAR(255) DEFAULT NULL,
+    `transp` VARCHAR(255) NOT NULL,
+    `rRule` VARCHAR(255) DEFAULT NULL,
     `start` VARCHAR(255) NOT NULL,
     `end` VARCHAR(255) NOT NULL,
     `drStamp` VARCHAR(255) NOT NULL,
-    `categories` VARCHAR(255) NULL,
-    `location` VARCHAR(255) NULL,
-    `geo` VARCHAR(255) NULL,
-    `description` VARCHAR(255) NULL,
-    `url` VARCHAR(255) NULL
+    `categories` VARCHAR(255) DEFAULT NULL,
+    `location` VARCHAR(255) DEFAULT NULL,
+    `geo` VARCHAR(255) DEFAULT NULL,
+    `description` TEXT DEFAULT NULL,
+    `url` VARCHAR(255) DEFAULT NULL,
+    INDEX (`id`)
 );
 
+-- Création de la table CalendarEventAssociation
 CREATE TABLE `CalendarEventAssociation` (
-    `eventId` VARCHAR(255) NOT NULL,
-    `calendarId` VARCHAR(255) NOT NULL,
+    `eventId` CHAR(36) NOT NULL,
+    `calendarId` CHAR(36) NOT NULL,
     PRIMARY KEY (`eventId`, `calendarId`),
     FOREIGN KEY (`eventId`) REFERENCES `Event`(`id`),
     FOREIGN KEY (`calendarId`) REFERENCES `Calendar`(`id`)
 );
-
-CREATE INDEX `idx_eventId` ON `Event` (`id`);
